3fe5f6ced77f79dcb0866b626f2ba11e2a1fb8b8
ky0yk <ksaik1024@gmail.com>
2021-06-25 08:03:07 +0900

    11.JSX を反映

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return <div>Hello,world!</div>
+//   }
+// }
 
 class App extends Component {
   render() {
-    return (
-      <div><h1>Hello,world!</h1></div>
+    return React.createElement(
+      "h1",
+      null,
+      "Hello,world!!"
     )
   }
 }
+
 
 export default App;
