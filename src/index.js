209ff9497e02a95dcf8d1fad070e63abbb76c084
ky0yk <ksaik1024@gmail.com>
2021-06-26 16:15:20 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reducers from './reducers';
+import App from './component/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducers)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Provider>,
+  document.getElementById('root'));
+registerServiceWorker();
